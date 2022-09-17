0babc4a9b02b6078ded9a27842e6cd1f825a844e
BabaYagaPROP <92880584+BabaYagaPROP@users.noreply.github.com>
2022-09-17 16:24:41 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <?php
-// Path: index.php
-//declaration of cookie name
-    $cookie_name = "znak";
-   
-    //if button value is 0 keep the same value of cookie
-    if(isset($_COOKIE[$cookie_name]) && isset($_POST['button']) && $_POST['button'] == 0 ){
-        setcookie($cookie_name, $_COOKIE[$cookie_name], time() + (86400 * 30), "/");
+//setting cookie for the first time
+if(!isset($_COOKIE['sign']))
+{
+    setcookie('sign', '1', time()+3600);
+}
+//if the cookie is set and button znam is pressed increment value of cookie
+if(isset($_COOKIE['sign']) && isset($_POST['button']) && $_POST['button'] == 1)
+{
+    setcookie('sign', $_COOKIE['sign']+1, time()+3600);
+    //if the value of cookie is greater than 6 set it to 1
+    if($_COOKIE['sign'] >= 6)
+    {
+        setcookie('sign', '1', time()+3600);
     }
-    //if cookie is set or button 'znam' is pressed change value of existing cookie
-    else if(isset($_COOKIE[$cookie_name]) && isset($_POST['button']) && $_POST['button'] == 1){
-        $rand = $_COOKIE[$cookie_name];
-        while($_COOKIE[$cookie_name] == $rand)
-            $rand = rand(1,6);
-        $cookie_value = $rand;
-        setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); 
-    }
-    //set cookie if it is not set yet
-    else if(!isset($_COOKIE[$cookie_name])){
-        //get random number and set it as cookie value
-        $rand = rand(1,6);
-        $cookie_value = $rand;
-        setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); 
-    }
+}
+//setting cookie which tells if site is loaded for the first time
+if(!isset($_POST['button']) || !isset($_COOKIE['first']))
+{
+    setcookie('first', '1', time()+3600);
+}
+else{
+    setcookie('first', '0', time()+3600);
+}
+
+
     
 ?>
 <!DOCTYPE html>		<!Znacznik deklaracji dokumentu html!>
@@ -44,15 +46,37 @@
 </div>					<!Zamkniecie pojemnika o nazwie "menu"!>
 <div id="content">		<!Utworzenie pojemnika o nazwie "content"!>
 <?php
-            // because of the way cookies work, we need to check if cookie is set or not
-            if(!isset($_COOKIE[$cookie_name]))
-                //if cookie is not set, display image with random number
-                echo "<img src='img/$cookie_value.png' alt='Znak'>";
-                //if cookie is set, display image with value of cookie
-            else if(isset($_COOKIE[$cookie_name]))
-                echo "<img src='img/$_COOKIE[$cookie_name].png' alt='Znak'>"; 
+//if button wasnt pressed yet display the first image
+if(!isset($_POST['button']) && isset($_COOKIE['sign']))
+{
+    echo "<img src='img/".$_COOKIE['sign'].".png' alt='znak' />";
+}
+//if button was pressed display the next image
+else if(isset($_COOKIE['sign'])&& isset($_POST['button']) && $_POST['button'] == 1)
+{
+    echo "<img src='img/".$_COOKIE['sign'].".png' alt='znak' />";
+}
+//if the cookie is set and button nie znam is pressed display the image with the answer
+else if(isset($_COOKIE['sign']) && isset($_POST['button']) && $_POST['button'] == 0)
+{
+    //if the page is loaded for the first time display the image with the answer
+    if($_COOKIE['first'] == 1)
+    {
+        echo "<img src='img/".($_COOKIE['sign']).".png' alt='znak' />";
+    }
+    //if the page is loaded for for more than one time display the image -1
+    else if($_COOKIE['sign'] == 1 && $_COOKIE['first'] == 0)
+    {
+        echo "<img src='img/6.png' alt='znak' />";
+    }
+    else if($_COOKIE['sign'] != 1 && $_COOKIE['first'] == 0)
+    {
+        echo "<img src='img/".($_COOKIE['sign']-1).".png' alt='znak' />";
+    }
+}
+
                  
-        ?>
+?>
 		<form action="index.php" method="post">			<!Utworzenie formularza: atrybut "action" określa, gdzie mają być wysyłane dane formularza podczas przesyłania formularza, metoda: post, czyli metoda przesyłania danych!>
             <button type="submit" value="1" name="button" id="yes">Znam</button>		<!Przycisk do przeslania odpowiedzi o wartosci "1", wyswietli sie odpowiedz o tresci: "Znam"!>
             <button type="submit" value="0" name="button" id="no">Nie znam</button>		<!Przycisk do przeslania odpowiedzi o wartosci "0", wyswietli sie odpowiedz o tresci: "Nie znam"!>
@@ -61,39 +85,43 @@
 		<br>
         <?php
         //if button 'znam' is pressed, display description of the sign
-            if(isset($_POST['button']) && $_POST['button'] == 0)
+        if(isset($_POST['button']) && $_POST['button'] == 0)
+        {
+            //switch case to display description of the sign
+            if($_COOKIE['first'])
+                $sign = $_COOKIE['sign'];
+            else
+                $sign = $_COOKIE['sign']-1;
+            switch($sign)
             {
-                //switch case to display description of the sign
-                switch($_COOKIE[$cookie_name])
-                {
-                    case 1:
-                        echo "<h2>Znak ostrzega przed miejscem na drodze szczególnie uczęszczanym przez dzieci w wieku od 7 do 15 lat. Znak umieszcza się zwłaszcza w pobliżu szkół podstawowych, gimnazjów, placówek prowadzących zajęcia z dziećmi, terenów zabaw itp.</h2>";
-                        break;
-                    case 2:
-                        echo "<h2>Znak ostrzega przed miejscem, w którym ruch jest kierowany za pomocą sygnalizacji świetlnej. Znak stosuje się:
-                        przed każdą sygnalizacją świetlną poza obszarem zabudowanym,
-                        przed pierwszą sygnalizacją w obszarze zabudowanym,
-                        w każdym miejscu, gdzie sygnalizacja zastosowana została do kierowania ruchem wahadłowym,
-                        w każdym miejscu, gdzie sygnalizator jest tylko zawieszony nad jezdnią.</h2>";
-                        break;
-                    case 3:
-                        echo "<h2>Znak ostrzega przed koniecznością ustąpienia pierwszeństwa. Umieszczony jest na drodze podporządkowanej przed skrzyżowaniem z drogą z pierwszeństwem, gdy spełnione są warunki widoczności (w innym wypadku stosuje się znak B-20 „stop”). W obrębie skrzyżowania znak dotyczy tylko najbliższej jezdni, przed którą został umieszczony. Umieszczony razem ze znakiem C-12 „ruch okrężny” określa pierwszeństwo dla znajdującego się na skrzyżowaniu. Może być umieszczony w innych miejscach, gdzie z przepisów ustawy – Prawo o ruchu drogowym wynika obowiązek ustąpienia pierwszeństwa, np. wyjazd z obiektu, torowisko tramwajowe.
-                        Pod znakiem może być umieszczona tabliczka T-6c „tabliczka wskazująca rzeczywisty przebieg drogi z pierwszeństwem przez skrzyżowanie”
-                        
-                        Na drogach o dopuszczalnej prędkości powyżej 60 km/h znak A-7 poprzedzany jest znakiem A-7 z tabliczką T-1 „tabliczka wskazująca odległość znaku ostrzegawczego od miejsca niebezpiecznego”.</h2>";
-                        break;
-                    case 4:
-                        echo "<h2>Znak ostrzega przed skrzyżowaniem z drogą podporządkowaną występującą po obu stronach drogi z pierwszeństwem. Stosowany jest poza obszarem zabudowanym dla wskazania pierwszeństwa drogi przebiegającej na wprost, przy której jest ustawiony. Jeżeli osie dróg poprzecznych nie przecinają się na skrzyżowaniu, stosuje się tabliczkę T-6b „tabliczka wskazująca układ dróg podporządkowanych”.</h2>";
-                        break;
-                    case 5:
-                        echo "<h2>Znak ostrzega przed pojedynczym niebezpiecznym zakrętem w prawo. Stosuje się go na ogół poza miastami, gdy kąt zwrotu łuku drogi jest większy niż 5°, a jego promień jest mniejszy niż 750 m oraz w zależności od przechyłki. Ustawiany także na zakrętach o ograniczonej widoczności, bądź gdzie dochodzi do częstych wypadków lub kolizji.</h2>";
-                        break;
-                    case 6:
-                        echo "<h2>Zwierzęta dzikie</h2>";
-                        break;
+                case 1:
+                    echo "<h2>Znak ostrzega przed miejscem na drodze szczególnie uczęszczanym przez dzieci w wieku od 7 do 15 lat. Znak umieszcza się zwłaszcza w pobliżu szkół podstawowych, gimnazjów, placówek prowadzących zajęcia z dziećmi, terenów zabaw itp.</h2>";
+                    break;
+                case 2:
+                    echo "<h2>Znak ostrzega przed miejscem, w którym ruch jest kierowany za pomocą sygnalizacji świetlnej. Znak stosuje się:
+                    przed każdą sygnalizacją świetlną poza obszarem zabudowanym,
+                    przed pierwszą sygnalizacją w obszarze zabudowanym,
+                    w każdym miejscu, gdzie sygnalizacja zastosowana została do kierowania ruchem wahadłowym,
+                    w każdym miejscu, gdzie sygnalizator jest tylko zawieszony nad jezdnią.</h2>";
+                    break;
+                case 3:
+                    echo "<h2>Znak ostrzega przed koniecznością ustąpienia pierwszeństwa. Umieszczony jest na drodze podporządkowanej przed skrzyżowaniem z drogą z pierwszeństwem, gdy spełnione są warunki widoczności (w innym wypadku stosuje się znak B-20 „stop”). W obrębie skrzyżowania znak dotyczy tylko najbliższej jezdni, przed którą został umieszczony. Umieszczony razem ze znakiem C-12 „ruch okrężny” określa pierwszeństwo dla znajdującego się na skrzyżowaniu. Może być umieszczony w innych miejscach, gdzie z przepisów ustawy – Prawo o ruchu drogowym wynika obowiązek ustąpienia pierwszeństwa, np. wyjazd z obiektu, torowisko tramwajowe.
+                    Pod znakiem może być umieszczona tabliczka T-6c „tabliczka wskazująca rzeczywisty przebieg drogi z pierwszeństwem przez skrzyżowanie”
+                    
+                    Na drogach o dopuszczalnej prędkości powyżej 60 km/h znak A-7 poprzedzany jest znakiem A-7 z tabliczką T-1 „tabliczka wskazująca odległość znaku ostrzegawczego od miejsca niebezpiecznego”.</h2>";
+                    break;
+                case 4:
+                    echo "<h2>Znak ostrzega przed skrzyżowaniem z drogą podporządkowaną występującą po obu stronach drogi z pierwszeństwem. Stosowany jest poza obszarem zabudowanym dla wskazania pierwszeństwa drogi przebiegającej na wprost, przy której jest ustawiony. Jeżeli osie dróg poprzecznych nie przecinają się na skrzyżowaniu, stosuje się tabliczkę T-6b „tabliczka wskazująca układ dróg podporządkowanych”.</h2>";
+                    break;
+                case 5:
+                    echo "<h2>Znak ostrzega przed pojedynczym niebezpiecznym zakrętem w prawo. Stosuje się go na ogół poza miastami, gdy kąt zwrotu łuku drogi jest większy niż 5°, a jego promień jest mniejszy niż 750 m oraz w zależności od przechyłki. Ustawiany także na zakrętach o ograniczonej widoczności, bądź gdzie dochodzi do częstych wypadków lub kolizji.</h2>";
+                    break;
+                case 0:
+                    echo "<h2>Zwierzęta dzikie</h2>";
+                    break;
 
-                }
             }
+        }
         ?>
 </div>		<!Zamkniecie pojemnika o nazwie "content"!>
 
